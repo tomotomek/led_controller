c519ab1d8892fa85c1a00c0421fe57aafbf50f99
BORDAC Tomas <tbordac@frequentis.com>
2019-11-11 10:33:43 +0100

    little cleanup, host rewritten to python

---
+++ host.py
@@ -0,0 +1,34 @@
+import sys
+import pyscreenshot as ImageGrab
+import numpy as np
+import requests
+from time import sleep
+
+# API_ENDPOINT = "http://192.168.0.36/"
+# API_ENDPOINT = sys.argv[1]
+# INTERVAL = sys.argv[2]
+API_ENDPOINT = "http://localhost:31480/"
+INTERVAL = 3
+
+def averageColorOfScreen():
+    image = ImageGrab.grab()
+    imageMatrix = np.array(image)
+    r = int(np.mean(imageMatrix[:,:,0]))
+    g = int(np.mean(imageMatrix[:,:,1]))
+    b = int(np.mean(imageMatrix[:,:,2]))
+    print([r, g, b])
+    data = {
+            'RGBcolor':[r,g,b]
+            } 
+    r = requests.post(url = API_ENDPOINT, data = data)
+    print('Server response: ', r.text)
+
+if __name__ == '__main__':
+
+  while True:
+    try:
+      averageColorOfScreen()
+      sleep(INTERVAL)
+    except:
+      print('Probably server at {} is disconnected'.format(API_ENDPOINT))
+      break
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const magic_home = require('magic-home');
 const express = require('express');
 const bodyParser = require('body-parser');
-const FastAverageColor = require('fast-average-color');
 
 const PORT = 31480;
 const app = express();
-const fac = new FastAverageColor();
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port: ${PORT}`)
@@ -14,9 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-	const img = req.body.img;
-	//const color = fac.getColor(img);
-
+	const img = req.body.RGBcolor;
 	console.log(img);
 	res.end('received');
 });
