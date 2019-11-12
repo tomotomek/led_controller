f39c32a9f7f265c38dea5a82d038f75012b0b937
tomotomek <tomas.bordac@gmail.com>
2019-11-12 15:29:15 +0100

    get IP addres from argument

--- server_LED_tape_controller.py
+++ server_LED_tape_controller.py
@@ -36,10 +36,10 @@ def run(server_class=HTTPServer, handler_class=S, port=8080):
 
 if __name__ == '__main__':
     from sys import argv
-    controller = MagicHomeApi('192.168.0.37', 1)
+    controller = MagicHomeApi(argv[1], 1)
     controller.turn_on()
 
     if len(argv) == 2:
-        run(port=int(argv[1]))
+        run(port=int(argv[2]))
     else:
         run()
